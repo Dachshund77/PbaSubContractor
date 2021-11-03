880cd44c9ebf422f20175a95b362d747105b5b73
Sven Büchner <sven.buchner@hansencx.com>
2021-11-03 09:32:13 +0100

    Introduces Product DataModel

---
+++ Src/DataModel/Product.cs
@@ -0,0 +1,10 @@
+namespace PbaSubContractor
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public long NumberInStock { get; set; }
+        public double SizeInKg { get; set; }
+    }
+}
