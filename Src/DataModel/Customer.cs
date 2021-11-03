293e980aa62733a0f7b688af191ad6191678d7b9
Sven Büchner <sven.buchner@hansencx.com>
2021-11-03 09:28:47 +0100

    Introduces Customer DataModel

---
+++ Src/DataModel/Customer.cs
@@ -0,0 +1,12 @@
+namespace PbaSubContractor
+{
+    public class Customer
+    {
+        public string Surname { get; set; }
+        public long CPR { get; set; }
+        public string Mail { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string CountryUnicode { get; set; }
+        public long CardNumber { get; set; }
+    }
+}
