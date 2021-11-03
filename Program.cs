dae7e9f4dd4c31e98a508572f7b0371c23853404
Sven Büchner <sven.buchner@hansencx.com>
2021-11-03 09:20:30 +0100

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PbaSubContractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
